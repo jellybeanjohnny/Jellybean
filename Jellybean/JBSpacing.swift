0657ee2bb0f7fa38f4033b0bf6fe200a86472fc6
jellybeanjohnny <mamerige@icloud.com>
2016-10-25 20:28:54 -0700

    Initial Commit

---
+++ Jellybean/JBSpacing.swift
@@ -0,0 +1,59 @@
+//
+//  JBSpacing.swift
+//  Jellybean
+//
+//  Created by Matt Amerige on 10/24/16.
+//  Copyright © 2016 BuildThings. All rights reserved.
+//
+
+import Foundation
+
+/**
+ JBSpacing provides the API through which the spacing for reviewing items is set.
+ There are only two public methods, increaseSpacing and decreaseSpacing,
+ and two public property, the daysUntilRepetition, and dueDate.
+ 
+ As items are recalled correctly the daysUntilRepetition roughly doubles, 
+ and if incorrectly recalled the value is halved.
+ 
+ This is a simple implementation based on the Leitner System for spaced repetition
+ 
+ */
+public class JBSpacing {
+  
+  /// Starts at zero
+  private(set) public var daysUntilRepetition = 0
+  
+  /// Starts at today
+  private(set) public var dueDate = Date()
+
+  
+  public init() {
+
+  }
+
+  
+  /// Call this after a card is recalled correctly to increment the spacing.
+  public func increaseSpacing() {
+    daysUntilRepetition = daysUntilRepetition * 2 + 1
+    adjustDueDate(forValue: daysUntilRepetition)
+  }
+  
+  /// Call  this after a card is recalled incorrectly to decrement the spacing
+  public func decreaseSpacing() {
+    // since this is integer division the answer will always discard any remainder, that is intentional.
+    daysUntilRepetition = daysUntilRepetition / 2
+    adjustDueDate(forValue: daysUntilRepetition)
+  }
+  
+  func adjustDueDate(forValue spacing: Int) {
+    
+    if spacing <= 0 {
+      dueDate = Date()
+    } else if let nextDueDate = Calendar.current.date(byAdding: .day, value: spacing, to: dueDate) {
+      dueDate = nextDueDate      
+    }
+  }
+
+  
+}
---
+++ Jellybean/Jellybean.h
@@ -0,0 +1,19 @@
+//
+//  Jellybean.h
+//  Jellybean
+//
+//  Created by Matt Amerige on 10/25/16.
+//  Copyright © 2016 BuildThings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jellybean.
+FOUNDATION_EXPORT double JellybeanVersionNumber;
+
+//! Project version string for Jellybean.
+FOUNDATION_EXPORT const unsigned char JellybeanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jellybean/PublicHeader.h>
+
+
